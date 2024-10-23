649de5a4d724cbe30dc4575c68c14d3b43902d3f
saikrishna321 <saikrishna321@yahoo.com>
2024-10-23 18:37:10 +0530

    fix build

--- src/FeatureSection.tsx
+++ src/FeatureSection.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { motion } from "framer-motion";
 import codeImage from "./assets/code.jpg";
 import CloudLogosSection from "./CloudLogosSection";
@@ -216,23 +215,5 @@ const FeatureSection = () => {
     </section>
   );
 };
-
-const FeatureItem = ({children}: { children: React.ReactNode }) => (
-    <li className="flex items-center space-x-3">
-      <svg
-          className="flex-shrink-0 w-8 h-8 text-[#5FD3F3]"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-            fillRule="evenodd"
-            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-            clipRule="evenodd"
-        ></path>
-      </svg>
-      <span>{children}</span>
-    </li>
-);
 
 export default FeatureSection;
